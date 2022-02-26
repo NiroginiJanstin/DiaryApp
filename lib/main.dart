9100ccf9b095c145784f635932bcac3a19a012fd
Nirogini Janstin <nirogini@smartzi.com>
2022-02-26 19:44:31 +0530

    Inital commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:diary_app/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: login()
+    );
+  }
+}
+
